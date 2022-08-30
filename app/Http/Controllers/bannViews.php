87619f054c15c9b1d3d15cc1b31b8d76c3f7b0da
HossameTor <hossametorchi07@gmail.com>
2022-08-31 00:47:49 +0100

    edit banniere

---
+++ app/Http/Controllers/bannViews.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Banniere;
+use Illuminate\Http\Request;
+
+class bannViews extends Controller
+{
+    public function count(Request $request){
+        $id = $request->banniereid; 
+        $bann = Banniere::find($id);
+        $bann->nb_vue->increment();
+    }
+}
